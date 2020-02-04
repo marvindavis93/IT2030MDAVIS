035d4d9df5f32ce481dc3ca2a120eab5edcc13f0
marvin davis <S01024543@acad.tri-c.edu>
2020-02-04 12:51:22 -0500

    lab3.2

--- Lab3/Lab3/Controllers/HomeController.cs
+++ Lab3/Lab3/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Lab3.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page..";
 
             return View();
         }
